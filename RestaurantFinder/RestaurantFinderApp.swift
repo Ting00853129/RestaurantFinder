f0a53e349acaef138cf4b79140aa85562abb4b41
Ting <yitingwu.1030@gmail.com>
2022-12-25 13:54:22 +0800

    Initial Commit

---
+++ RestaurantFinder/RestaurantFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantFinderApp.swift
+//  RestaurantFinder
+//
+//  Created by Victor Tao on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
